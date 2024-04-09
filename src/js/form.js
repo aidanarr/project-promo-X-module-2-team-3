5919d24d2433a5bb9319deb6cb9f2dec6ff79d42
aidanar <aidanarfer@outlook.es>
2024-04-09 12:33:25 +0200

    add card preview js

---
+++ src/js/form.js
@@ -0,0 +1,18 @@
+form.addEventListener("input", (event)=> {
+    const input = event.target.id;
+    const valueInput = event.target.value;
+
+    if (input === "name") {
+        namePreview.innerHTML = valueInput;
+    } else if (input === "position") {
+        jobPreview.innerHTML = valueInput;
+    } else if (input === "phone") {
+        telPreview.href = valueInput;
+    } else if (input === "email") {
+        mailPreview.href = valueInput;
+    } else if (input === "link") {
+        linkedinPreview.href = valueInput;
+    } else if (input === "gituser") {
+        githubPreview.href = valueInput;
+    }
+});
---
+++ src/js/var.js
@@ -0,0 +1,7 @@
+const form = document.querySelector(".js_form");
+const namePreview = document.querySelector(".js_name");
+const jobPreview = document.querySelector(".js_job");
+const telPreview = document.querySelector(".js_tel");
+const mailPreview = document.querySelector(".js_mail");
+const linkedinPreview = document.querySelector(".js_linkedin");
+const githubPreview = document.querySelector(".js_github");
